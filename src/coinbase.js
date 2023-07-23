0256d6f394b1648dce23066146a9444c592abead
Julián Gelvez <gelvez30@gmail.com>
2023-07-23 19:48:59 -0300

    cambio de coinbase

--- src/coinbase.js
+++ src/coinbase.js
@@ -1,15 +1,25 @@
 const TransaccionLeafAbstracta = require('./transaccionLeafAbstracta');
+const { MD5HashingStrategy } = require('./hashingStrategy');
+const { v4: uuidv4 } = require('uuid');
 
 class Coinbase extends TransaccionLeafAbstracta {
-  constructor(TKN, OUT) {
-    super(null, OUT);
-    this.TKN = TKN;
+  constructor(OUT) {
+    super(`tx-${uuidv4()}`, OUT);
+    this.TKN = `TKN-${uuidv4()}`;
+    this.generarHashPorDefecto();
+  }
+
+  generarHashPorDefecto() {
+    const data = `${this.TKN}-${this.OUT}-${this.id}`;
+    const md5HashingStrategy = new MD5HashingStrategy();
+    this.setHashStrategy(md5HashingStrategy);
+    this.hash = this.hashStrategy.generateHash(data);
   }
 
   agregarTransaccion(transaccion) {
     throw new Error('No se pueden agregar transacciones a una transacción de tipo Coinbase.');
   }
-  
+
   obtenerNivel() {
     return 0;
   }
