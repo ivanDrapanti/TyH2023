4f7e32027f7f709875fe899a443e7911122bd550
Julián Gelvez <gelvez30@gmail.com>
2023-07-30 17:12:07 -0300

    cambios coinbase,leaf y abstracta

--- src/transaccionAbstracta.js
+++ src/transaccionAbstracta.js
@@ -1,31 +1,53 @@
-const { MD5HashingStrategy } = require('./hashingStrategy');
+const { MD5HashingStrategy, SHA256HashingStrategy } = require('./hashingStrategy');
 const { v4: uuidv4 } = require('uuid');
 
 class TransaccionAbstracta {
-  
-  constructor(idAnterior = null) {
+  constructor(idAnterior = null, OUT) {
     this.id = `tx-${uuidv4()}`;
     this.IN = idAnterior; // Almacena el id de la transacción anterior
+    this.OUT = OUT;
     this.hash = null;
+    this.hashStrategy = new MD5HashingStrategy(); // Estrategia de hashing por defecto (MD5)
   }
 
-  generarHashPorDefecto() {
-    
-    const data = `${this.TKN || ''}-${this.OUT}-${this.id}-${this.IN || ''}`;
+  calcularHash() {
+    if (!this.hashStrategy) {
+      throw new Error('Hash strategy no se ha generado.');
+    }
 
-    const md5HashingStrategy = new MD5HashingStrategy();
-    this.setHashStrategy(md5HashingStrategy);
+    const data = `${this.TKN || ''}-${this.OUT}-${this.id}-${this.IN || ''}`;
     this.hash = this.hashStrategy.generateHash(data);
   }
 
-    agregarTransaccion(transaccion) {
-      throw new Error('El método agregarTransaccion debe ser implementado por las clases hijas.');
-    }
-  
-    obtenerNivel() {
-      throw new Error('El método obtenerNivel debe ser implementado por las clases hijas.');
-    }
+  setHashStrategy(hashStrategy) {
+    this.hashStrategy = hashStrategy;
   }
-  
-  module.exports = TransaccionAbstracta;
-  
+
+  validarIntegridad() {
+    const data = `${this.TKN || ''}-${this.OUT}-${this.id}-${this.IN || ''}`;
+    const computedHash = this.hashStrategy.generateHash(data);
+    return this.hash === computedHash;
+  }
+
+  cambiarHashAMd5() {
+    const md5HashingStrategy = new MD5HashingStrategy();
+    this.setHashStrategy(md5HashingStrategy);
+    this.calcularHash();
+  }
+
+  cambiarHashASHA256() {
+    const sha256HashingStrategy = new SHA256HashingStrategy();
+    this.setHashStrategy(sha256HashingStrategy);
+    this.calcularHash();
+  }
+
+  agregarTransaccion(transaccion) {
+    throw new Error('El método agregarTransaccion debe ser implementado por las clases hijas.');
+  }
+
+  obtenerNivel() {
+    throw new Error('El método obtenerNivel debe ser implementado por las clases hijas.');
+  }
+}
+
+module.exports = TransaccionAbstracta;
--- src/transaccionLeafAbstracta.js
+++ src/transaccionLeafAbstracta.js
@@ -1,16 +1,8 @@
-const { v4: uuidv4 } = require('uuid');
 const TransaccionAbstracta = require('./transaccionAbstracta');
 
 class TransaccionLeafAbstracta extends TransaccionAbstracta {
-  
   constructor(IN, OUT) {
     super();
-    this.IN = IN;
-    this.OUT = OUT;
-  }
-  
-  calcularHash(hashStrategy) {
-    this.hash = hashStrategy.generateHash(this);
   }
 }
 
