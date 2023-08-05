00d9ef0e2f79e2eb17b577122a9274118c73d466
Julián Gelvez <gelvez30@gmail.com>
2023-08-05 20:23:05 -0300

    Cambios en Nodo.test

--- test/Nodos.test.js
+++ test/Nodos.test.js
@@ -2,6 +2,7 @@ const Blockchain = require("../src/Blockchain");
 const BloqueAbierto = require("../src/bloque/BloqueAbierto");
 const TransaccionSimple = require("../src/transaccion/TransaccionSimple");
 const Nodo = require("../src/Nodo");
+const BloqueCerrado = require("../src/bloque/BloqueCerrado");
 
 describe('Nodo', () => {
 	test('Debe crear un Nodo correctamente', () => {
@@ -12,6 +13,24 @@ describe('Nodo', () => {
 
 		//Verificar que no se crea un nodo con la blockChain nula.
 		expect(() => new Nodo(null, bloqueAbierto)).toThrow("BlockChain debe ser distinto de nulo");
+	});
+
+	describe('BloqueCerrado', () => {
+		test('should throw error when hash strategy is not generated', () => {
+			const timeStamp = 1630438295000; // Replace with the desired timestamp
+			const previousHash = 'previousHash'; // Replace with the desired previous hash
+			const transacciones = ['transaccion1', 'transaccion2']; // Replace with your transaction data
+
+			// Create a BloqueCerrado instance without setting the hash strategy
+			const bloqueCerrado = new BloqueCerrado(transacciones, timeStamp, previousHash);
+			bloqueCerrado.hashStrategy=null;
+			// Expect an error to be thrown when calculating the hash
+			expect(() => {
+			bloqueCerrado.calcularHash();
+			}).toThrowError('Hash strategy no se ha generado.');
+		});
+
+	// ... other test cases ...
 	});
 
 	test('Debe generar 2 bloques y agregarlo a la BlockChain', () => {
